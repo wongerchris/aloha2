c0e9ca8f35912774a5ae019793e4fb57028276e5
wongerchris <wongerchris@gmail.com>
2016-07-17 20:49:31 -0700

    initial commit for aloha project 2

---
+++ README.md
@@ -0,0 +1,13 @@
+###REDacademy Second Project
+
+##Aloha 2:
+
+###What was added:
+
+1. Mobile first approach
+2. Removal of all floats and Flexbox implementation 
+3. jQuery - smooth scroll, email validation/alert popup, add to cart clicker
+4. jQuery slider - Flickity Slider
+5. Flexbox grid with proportional box sizing with responsive design
+
+
---
+++ js/main.js
@@ -0,0 +1,51 @@
+//Smooth Scroll
+  $(document).ready(function(){
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 800);
+            return false;
+          }
+        }
+      });
+    });
+  });
+
+//Email Alert Message
+  $(document).ready(function(){
+    $(function() {
+      $('#updates').on('submit', 'form', function(event) {
+        event.preventDefault();
+        var $emailInput = $('#email-input')
+
+        if ( $emailInput.val().length > 5 ) {
+          alert($emailInput.val() + " -> Aloha thanks you for subscribing :)");
+          $emailInput.val('');
+          $( "#message" ).text( "YAAAA thats what I am talking about" ).show().fadeOut( 2200 );
+          event.preventDefault();
+        } else {
+          alert('Please submit a valid email address.');
+          $( "#message" ).text( "Not valid email!" ).show().fadeOut( 2200 );
+          event.preventDefault();
+        }
+      });
+    });
+  });
+
+//Add to cart
+$(document).ready(function() {
+  var counter = 0; 
+    $(".addMe").click(function(){
+        counter++;
+
+    $("#counter").text(counter);
+  });
+});
+
+
+
